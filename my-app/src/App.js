055f7058830b5204c267a680d4a290676137fbe0
Mason <star.wang@live.com>
2019-11-04 16:16:45 +1300

    added clock into the lifecycle

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    let now  = new Date();
+    let formattedString = this.formatTime(now);
+
+    this.state = {
+      formattedTime: formattedString
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    if(this.timerID) {
+      clearInterval(this.timerID);
+    }
+  }
+
+  tick() {
+    let now  = new Date();
+    let formattedString = this.formatTime(now);
+
+    this.setState({
+      formattedTime: formattedString
+    });
+  }
+
+  formatTime(time) {
+    const is24HourTime = this.props.time_24Hour;
+    let formattedString = '';
+    if (is24HourTime) {
+      formattedString = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
+    } else {
+      formattedString = time.toLocaleTimeString();
+    }
+    return formattedString;
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>It is {this.state.formattedTime}.</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
