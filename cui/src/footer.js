3ef36bb4b9d474642cc2a299ff808c0b87c60c0f
Mason <m.wang@coherent-solutions.com>
2023-06-02 20:21:16 +1200

    overwritten container max-width

--- cui/src/footer.js
+++ cui/src/footer.js
@@ -1,10 +1,8 @@
 import React from "react"
 import "./footer.scss";
 
-const CFooter = () => <footer className="footer font-small">
+const CFooter = () => 
     <div className="footer-copyright text-center py-1">© 2023 Copyright: <a href="#">Mr. Wang</a>
     </div>
-
-</footer>
 
 export default CFooter
