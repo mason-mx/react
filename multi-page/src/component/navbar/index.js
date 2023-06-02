47da3462a33df8acf6deff0833f6c4b30e924c42
Mason <m.wang@coherent-solutions.com>
2023-06-02 19:16:26 +1200

    updated navbar of multi-page

--- multi-page/src/component/navbar/index.js
+++ multi-page/src/component/navbar/index.js
@@ -1,27 +1,16 @@
 import React from "react";
-import { Nav, NavLink, NavMenu }
-	from "./navbarelement";
+import { Nav, NavLink, NavMenu } from "./navbarelement";
 
 const Navbar = () => {
 	return (
 		<>
 			<Nav>
 				<NavMenu>
-					<NavLink to="/" activeStyle>
-						Home
-					</NavLink>
-					<NavLink to="/about" activeStyle>
-						About
-					</NavLink>
-					<NavLink to="/contact" activeStyle>
-						Contact Us
-					</NavLink>
-					<NavLink to="/blogs" activeStyle>
-						Blogs
-					</NavLink>
-					<NavLink to="/sign-up" activeStyle>
-						Sign Up
-					</NavLink>
+					<NavLink to="/" activeStyle>Home</NavLink>
+					<NavLink to="/about" activeStyle>About it</NavLink>
+					<NavLink to="/contact" activeStyle>Contact Us</NavLink>
+					<NavLink to="/blogs" activeStyle>Blogs</NavLink>
+					<NavLink to="/sign-up" activeStyle>Sign Up</NavLink>
 				</NavMenu>
 			</Nav>
 		</>
